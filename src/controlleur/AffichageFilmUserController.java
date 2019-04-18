507110aaa8c0061eaf4d96439100b3c29ca8e62d
HoussemBouzid <houssem.bouzid@esprit.tn>
2019-04-18 02:14:00 +0100

    liste film te5dem

--- src/controlleur/AffichageFilmUserController.java
+++ src/controlleur/AffichageFilmUserController.java
@@ -9,19 +9,25 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import services.FilmServices;
+import controlleur.ListeFilm;
+import entities.Film;
 
 /**
  * FXML Controller class
  *
  * @author Houssem
  */
-public class ListeFilmUserController implements Initializable {
+public class AffichageFilmUserController implements Initializable {
 
     @FXML
-    private JFXListView<?> table;
+    private JFXListView<Film> table;
     @FXML
     private JFXTextField text_id;
     @FXML
@@ -35,6 +41,20 @@ public class ListeFilmUserController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        try {
+
+            services.FilmServices Fs = new FilmServices();
+
+            table.getItems().addAll(Fs.FetchAll());
+
+            table.setCellFactory(lv -> new ListeFilm());
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ListesFilmsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }    
     
 }
